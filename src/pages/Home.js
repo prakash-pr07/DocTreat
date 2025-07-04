e9e4225f6e6b05992c8ec815106ff4ffd6bfee35
prakash_07 <prakashranjan8454@gmail.com>
2025-07-04 12:50:58 +0530

    Name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="bg-yellow-200 min-h-screen flex justify-center items-center">
       <h1 className="text-3xl font-bold text-gray-800 text-center">
-        Welcome to LoGic Legal. Please Sign Up or Login.
+        Welcome to DocTreat. Please Sign Up or Login.
       </h1>
     </div>
   );
