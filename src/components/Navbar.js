f696fdce089079b89c5551b3e30d6c04c7674bba
prakash_07 <prakashranjan8454@gmail.com>
2025-07-04 12:51:57 +0530

    Name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="bg-blue-300 text-white p-4 flex justify-between items-center shadow-md">
-      <h1 className="text-xl font-bold">LoGic Legal</h1>
+      <h1 className="text-xl font-bold">DocTreat</h1>
       <div className="flex gap-4">
         <Link to="/" className="bg-blue-800 px-4 py-1 rounded hover:bg-blue-900">Home</Link>
         <Link to="/signup" className="bg-blue-800 px-4 py-1 rounded hover:bg-blue-900">Sign Up</Link>
