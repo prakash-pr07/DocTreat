8ec35f3cd3eb91302246cdfed184d7b12c081fca
prakash_07 <prakashranjan8454@gmail.com>
2025-07-08 22:16:08 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import SignupPage from "./pages/SignUp";
 import Login from "./pages/Login";
 import DoctorDashboard from "./pages/DoctorDashboard";
 import PatientDashboard from "./pages/PatientDashboard";
-
+import ConnectDoctors from "./pages/ConnectDoctors";
+import DoctorProfile from "./pages/DoctorProfile"; 
+import AdminDashboard from "./pages/AdminDashboard";
+import AskAI from "./pages/AskAI"; 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";  // Also make sure this is imported
 
@@ -21,9 +24,13 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/ask-ai" element={<AskAI />} />
+          <Route path="/connect-doctors" element={<ConnectDoctors />} />
+          <Route path="/doctor/:id" element={<DoctorProfile />} />
             {/* Role-based dashboards */}
           <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
           <Route path="/patient/dashboard" element={<PatientDashboard />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
         </Routes>
       </div>
 
